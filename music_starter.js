7b7925223c89fe55590e479ecf71a8b6522e599c
Braden-T-Thomas <braden.t.thomas@gmail.com>
2024-02-05 10:58:15 +1300

    Changes I guess?

--- music_starter.js
+++ music_starter.js
@@ -13,7 +13,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   let key_height_list = [];
 
   let key_width = width / key_count;
-  let key_push = map(other, 0, 100, 0, 50);
+  let key_push = map(other, 0, 100, 0, 35);
   
 
   textAlign(CENTER);
@@ -30,32 +30,52 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     fill("#faf8f9");
     stroke(0);
     key_list.push(rect(key_x_pos, key_height_list[i], key_width, 50));
-
-    fill("#11120d");
-    rect(width/2, height - 275, width+10, 50);
-    
-    quad(15, height - 250, 25, height - 25, 55, height - 25, 65, height - 250)
-    rect(40, height - 250, 60, 40);
-    
-    quad(width - 15, height - 250,  width - 25, height - 25, width - 55, height - 25, width -65, height - 250);
-    rect(width - 40, height - 250, 60, 40);
-
-    rect(width/2, height - 260, 350, 40);
-    rect(width/2, height - 240, 175, 75);
-
-    quad(width/2 - 75, height - 240, width/2 - 65, height - 20, width/2 - 45, height - 20, width/2 - 55, height - 240);
-    quad(width/2 + 75, height - 240, width/2 + 65, height - 20, width/2 + 45, height - 20, width/2 + 55, height - 240);
-    rect(width/2, height, 130, 50);
-
-
-    stroke("#11120d");
-    fill("#a1925d");
-    quad(20, height - 25, 20, height, 60, height, 60, height - 35);
-    quad(width - 20, height - 25, width - 20, height, width - 60, height, width - 60, height - 35);
   }
 
-  var key_select = Math.floor(Math.random * (key_list.length - 0) + 0);
-  //key_height_list[1] += key_push
-  //stroke("#9a4138");
-  //line(0, height-300, width, height-300);
+  
+
+
+  fill("#11120d");
+  rect(width/2, height - 275, width+10, 50);
+  
+  stroke("FFFFFF");
+  strokeWeight(7);
+  line(width/2 - 40, height - 200, width/2 - 30, height - 25);
+  line(width/2 + 40, height - 200, width/2 + 30, height - 25);
+  line(width/2, height - 200, width/2, height - 25);
+
+  fill("#11120d");
+  stroke("#11120d");
+  strokeWeight(0.5);
+  quad(15, height - 250, 25, height - 25, 55, height - 25, 65, height - 250)
+  rect(40, height - 250, 60, 40);
+  
+  quad(width - 15, height - 250,  width - 25, height - 25, width - 55, height - 25, width -65, height - 250);
+  rect(width - 40, height - 250, 60, 40);
+
+  rect(width/2, height - 260, 350, 40);
+  rect(width/2, height - 240, 175, 75);
+
+  quad(width/2 - 75, height - 240, width/2 - 65, height - 20, width/2 - 45, height - 20, width/2 - 55, height - 240);
+  quad(width/2 + 75, height - 240, width/2 + 65, height - 20, width/2 + 45, height - 20, width/2 + 55, height - 240);
+  rect(width/2, height, 130, 50);
+
+  
+
+  stroke("#11120d");
+  fill("#a1925d");
+  quad(20, height - 25, 20, height, 60, height, 60, height - 35);
+  quad(width - 20, height - 25, width - 20, height, width - 60, height, width - 60, height - 35);
+
+
+  // Use map to select the keys
+
+  //key_height_list[0] + 50;
+  //key_height_list[map(vocals, 0, 100, 0, key_list.length())] + key_push;
+  
+
+  //Ideas
+  // Pillars on the side behind piano to frame it
+  // Background gradient from light at the bottom to black at the top
+  // Use a randomizer to assign the keys different maps to have dynamics
 }
