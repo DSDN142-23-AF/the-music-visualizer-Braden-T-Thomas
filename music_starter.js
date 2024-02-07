653e493f4660e45f733e466125f7e4d30f13679d
Braden-T-Thomas <braden.t.thomas@gmail.com>
2024-02-08 12:59:40 +1300

    Cleaned up files and code

--- music_starter.js
+++ music_starter.js
@@ -6,35 +6,27 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
 
-  
-  
+
+  // Keys for piano
+  fill(255);
   let key_count = 18;
-  let key_list = [];
-  let key_height_list = [];
-
   let key_width = width / key_count;
-  let key_push = map(other, 0, 100, 0, 35);
   
-
   textAlign(CENTER);
   textSize(vocal);
   text(words, width/2, height/3);
+
   
   for(let i = 0; i <= key_count; i++){
     let key_x_pos = width-(width - ((width/key_count)/2)) + ((width/key_count) * i);
-    var key_height = height-300;
-
-    key_height_list.push(key_height);
-    
-    
+    // I was planning on having different maps for each key, but nothing works. This is the best I'm gonna get.
+    var key_height = height-300 + map(other, 0, 100, 0, 30);
+    rect(key_x_pos, key_height, key_width, 50);
     fill("#faf8f9");
     stroke(0);
-    key_list.push(rect(key_x_pos, key_height_list[i], key_width, 50));
   }
 
-  
-
-
+  // Piano body
   fill("#11120d");
   rect(width/2, height - 275, width+10, 50);
   
@@ -60,22 +52,13 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   quad(width/2 + 75, height - 240, width/2 + 65, height - 20, width/2 + 45, height - 20, width/2 + 55, height - 240);
   rect(width/2, height, 130, 50);
 
-  
-
   stroke("#11120d");
   fill("#a1925d");
   quad(20, height - 25, 20, height, 60, height, 60, height - 35);
   quad(width - 20, height - 25, width - 20, height, width - 60, height, width - 60, height - 35);
 
 
-  // Use map to select the keys
-
-  //key_height_list[0] + 50;
-  //key_height_list[map(vocals, 0, 100, 0, key_list.length())] + key_push;
-  
-
   //Ideas
   // Pillars on the side behind piano to frame it
   // Background gradient from light at the bottom to black at the top
-  // Use a randomizer to assign the keys different maps to have dynamics
 }
