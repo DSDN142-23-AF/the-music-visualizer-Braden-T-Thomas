77d986809ae26740ed7fadb8549e9a443af0ab0a
Braden-T-Thomas <braden.t.thomas@gmail.com>
2024-02-08 13:37:20 +1300

    Added pillars

--- music_starter.js
+++ music_starter.js
@@ -1,13 +1,80 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background("#DCDCDC");
+  background("#63666d");
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
 
 
+
+  
+  // left pillar
+  fill("#dcddd8");
+  stroke("#444540");
+  rect(0, height/2, 165, height - 200);
+
+  // left pillar grooves
+  stroke("#b7b4aa");
+  strokeWeight(20);
+  line(0, height - 120, 0, 120);
+  line(30, height - 120, 30, 120);
+  line(60, height - 120, 60, 120);
+
+  // left intermediate plates
+  stroke("#6c6a5e");
+  strokeWeight(40);
+  line(0, 80, 80, 80);
+  line(0, height- 80, 80, height - 80);
+
+  // left plates
+  strokeWeight(20);
+  line(0, 10, 110, 10);
+  line(0, height - 10, 110, height - 10);
+
+  // left intermediate quad
+  strokeWeight(1);
+  fill("#dcddd8");
+  stroke("#444540");
+  quad(0, height - 60, 0, height - 20, 110, height - 20, 85, height - 60);
+  quad(0, 60, 0, 20, 110, 20, 85, 60);
+
+
+  // right pillar
+  stroke(0);
+  stroke("#444540");
+  fill("#dcddd8");
+  rect(width, height/2, 165, height - 200);
+
+  // right pillar grooves
+  stroke("#b7b4aa");
+  strokeWeight(20);
+  line(width, height - 120, width, 120);
+  line(width - 30, height - 120, width - 30, 120);
+  line(width - 60, height - 120, width - 60, 120);
+
+  // right intermediate plates
+  stroke("#6c6a5e");
+  strokeWeight(40);
+  line(width, 80, width - 80, 80);
+  line(width, height- 80, width - 80, height - 80);
+
+  // right plates
+  strokeWeight(20);
+  line(width, 10, width - 110, 10);
+  line(width, height - 10, width - 110, height - 10);
+
+  // right intermediate quad
+  strokeWeight(1);
+  fill("#dcddd8");
+  stroke("#444540");
+  quad(width, height - 60, width, height - 20, width - 110, height - 20, width - 85, height - 60);
+  quad(width, 60, width, 20, width - 110, 20, width - 85, 60);
+
+
   // Keys for piano
+  stroke(0);
+  strokeWeight(1);
   fill(255);
   let key_count = 18;
   let key_width = width / key_count;
@@ -20,7 +87,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   for(let i = 0; i <= key_count; i++){
     let key_x_pos = width-(width - ((width/key_count)/2)) + ((width/key_count) * i);
     // I was planning on having different maps for each key, but nothing works. This is the best I'm gonna get.
-    var key_height = height-300 + map(other, 0, 100, 0, 30);
+    var key_height = height-300 + map(other, 0, 100, 0, 25);
     rect(key_x_pos, key_height, key_width, 50);
     fill("#faf8f9");
     stroke(0);
