506780f505284c947273e5eb8aa40536b61f040e
Braden-T-Thomas <braden.t.thomas@gmail.com>
2024-02-10 12:15:42 +1300

    Changed a few things

--- music_starter.js
+++ music_starter.js
@@ -6,9 +6,83 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
 
+  // background
+  var gradient = ["#414b57", "#444e5b", "#46525e", "#495561", "#4c5865", "#4e5b68", "#515f6c", "#54626f", "#566573",
+"#596977", "#5c6c7a", "#5e707e", "#617381"];
+  let gradient_count = 12;
+  for(let i = 0; i <= gradient_count; i++){
+    fill(gradient[i]);
+    stroke(gradient[i]);
 
+    let gradient_rect_y_pos = height/gradient_count * i;
+    rect(width/2, gradient_rect_y_pos, width, height/gradient_count);
+  }
 
   
+  draw_pillars();
+  draw_hole();
+
+  
+
+  // Keys for piano
+  stroke(0);
+  strokeWeight(1);
+  fill(255);
+  let key_count = 18;
+  let key_width = width / key_count;
+  
+  textAlign(CENTER);
+  textSize(vocal);
+  text(words, width/2, height/3);
+
+  
+  for(let i = 0; i <= key_count; i++){
+    let key_x_pos = width-(width - ((width/key_count)/2)) + ((width/key_count) * i);
+    // I was planning on having different maps for each key, but nothing works. This is the best I'm gonna get.
+    var key_height = height-300 + map(other, 0, 100, 0, 25);
+    rect(key_x_pos, key_height, key_width, 50);
+    fill("#faf8f9");
+    stroke(0);
+  }
+
+  // Piano body
+  fill("#11120d");
+  rect(width/2, height - 275, width+10, 50);
+  
+  stroke("FFFFFF");
+  strokeWeight(7);
+  line(width/2 - 40, height - 200, width/2 - 30, height - 25);
+  line(width/2 + 40, height - 200, width/2 + 30, height - 25);
+  line(width/2, height - 200, width/2, height - 25);
+
+  fill("#11120d");
+  stroke("#11120d");
+  strokeWeight(0.5);
+  quad(15, height - 250, 25, height - 25, 55, height - 25, 65, height - 250)
+  rect(40, height - 250, 60, 40);
+  
+  quad(width - 15, height - 250,  width - 25, height - 25, width - 55, height - 25, width -65, height - 250);
+  rect(width - 40, height - 250, 60, 40);
+
+  rect(width/2, height - 260, 350, 40);
+  rect(width/2, height - 240, 175, 75);
+
+  quad(width/2 - 75, height - 240, width/2 - 65, height - 20, width/2 - 45, height - 20, width/2 - 55, height - 240);
+  quad(width/2 + 75, height - 240, width/2 + 65, height - 20, width/2 + 45, height - 20, width/2 + 55, height - 240);
+  rect(width/2, height, 130, 50);
+
+  stroke("#11120d");
+  fill("#a1925d");
+  quad(20, height - 25, 20, height, 60, height, 60, height - 35);
+  quad(width - 20, height - 25, width - 20, height, width - 60, height, width - 60, height - 35);
+
+
+  //Ideas
+  // Pillars on the side behind piano to frame it
+  // Background gradient from light at the bottom to black at the top
+}
+
+function draw_pillars(){
   // left pillar
   fill("#dcddd8");
   stroke("#444540");
@@ -70,62 +144,29 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   stroke("#444540");
   quad(width, height - 60, width, height - 20, width - 110, height - 20, width - 85, height - 60);
   quad(width, 60, width, 20, width - 110, 20, width - 85, 60);
+}
 
-
-  // Keys for piano
-  stroke(0);
-  strokeWeight(1);
-  fill(255);
-  let key_count = 18;
-  let key_width = width / key_count;
-  
-  textAlign(CENTER);
-  textSize(vocal);
-  text(words, width/2, height/3);
-
-  
-  for(let i = 0; i <= key_count; i++){
-    let key_x_pos = width-(width - ((width/key_count)/2)) + ((width/key_count) * i);
-    // I was planning on having different maps for each key, but nothing works. This is the best I'm gonna get.
-    var key_height = height-300 + map(other, 0, 100, 0, 25);
-    rect(key_x_pos, key_height, key_width, 50);
-    fill("#faf8f9");
-    stroke(0);
-  }
-
-  // Piano body
-  fill("#11120d");
-  rect(width/2, height - 275, width+10, 50);
-  
-  stroke("FFFFFF");
+function draw_hole(){
+  ellipseMode(CENTER);
+  fill("#a10e05");
+  stroke("#a10e05");
+  strokeWeight(3);
+  line(width/2 + 23, height - 575, width/2 + 23, height - 660)
   strokeWeight(7);
-  line(width/2 - 40, height - 200, width/2 - 30, height - 25);
-  line(width/2 + 40, height - 200, width/2 + 30, height - 25);
-  line(width/2, height - 200, width/2, height - 25);
+  line(width/2 + 12, height - 515, width/2 + 12, height - 660)
+  strokeWeight(5);
+  line(width/2 + 3, height - 545, width/2 + 3, height - 660)
+  strokeWeight(8);
+  line(width/2 - 2, height - 565, width/2 - 2, height - 660)
+  strokeWeight(6);
+  line(width/2 - 12, height - 495, width/2 - 12, height - 660)
+  strokeWeight(5);
+  line(width/2 + 26, height - 505, width/2 + 26, height - 660)
 
-  fill("#11120d");
-  stroke("#11120d");
   strokeWeight(0.5);
-  quad(15, height - 250, 25, height - 25, 55, height - 25, 65, height - 250)
-  rect(40, height - 250, 60, 40);
-  
-  quad(width - 15, height - 250,  width - 25, height - 25, width - 55, height - 25, width -65, height - 250);
-  rect(width - 40, height - 250, 60, 40);
+  ellipse(width/2, height - 690, 150);
 
-  rect(width/2, height - 260, 350, 40);
-  rect(width/2, height - 240, 175, 75);
-
-  quad(width/2 - 75, height - 240, width/2 - 65, height - 20, width/2 - 45, height - 20, width/2 - 55, height - 240);
-  quad(width/2 + 75, height - 240, width/2 + 65, height - 20, width/2 + 45, height - 20, width/2 + 55, height - 240);
-  rect(width/2, height, 130, 50);
-
-  stroke("#11120d");
-  fill("#a1925d");
-  quad(20, height - 25, 20, height, 60, height, 60, height - 35);
-  quad(width - 20, height - 25, width - 20, height, width - 60, height, width - 60, height - 35);
-
-
-  //Ideas
-  // Pillars on the side behind piano to frame it
-  // Background gradient from light at the bottom to black at the top
+  fill(0);
+  stroke(0);
+  ellipse(width/2, height - 700, 150);
 }
